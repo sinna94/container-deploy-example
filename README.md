e4a56c71a4bdcad52589fba425ce988a71cf1189
chunghyun lee <sinna94@naver.com>
2023-03-15 22:53:51 +0900

    nginx 로 reverse proxy 구성

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # spring-boot-k8s
+
+## docker-compose 로 단일 api 실행
+```shell
+$ docker compose up -d
+
+# 조회
+$ curl http://localhost:8080/hello
+> hello, -api%      
+```
+## docker-compose 로 다중 api 실행
+nginx 로 reverse proxy 를 구성하여 load balancing 
+```shell
+$ cd docker-compose-load-balancer
+$ docker compose up -d
+
+# 조회
+$ curl http://localhost/api/hello
+> hello, api-1
+$ curl http://localhost/api/hello
+> hello, api-2
+$ curl http://localhost/api/hello
+> hello, api-3    
+```
--- src/main/kotlin/chung/me/springbootk8s/HelloController.kt
+++ src/main/kotlin/chung/me/springbootk8s/HelloController.kt
@@ -1,5 +1,6 @@
 package chung.me.springbootk8s
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("hello")
 class HelloController {
+    @Value("\${spring.application.name}")
+    lateinit var appName: String
 
     @GetMapping
     fun hello(): ResponseEntity<String> {
-        return ResponseEntity.ok("hello")
+        return ResponseEntity.ok("hello, $appName")
     }
 }
